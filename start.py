77e3bd8c259255d53d3670f25972e607f05e73e7
hanifamin16 <hanifamin16@gmail.com>
2023-08-14 07:00:41 +0800

    practicing the art of making animation

--- start.py
+++ start.py
@@ -1,6 +1,5 @@
 from manim import *
 
-
 class CreateCircle(Scene):
     def construct(self):
         circle = Circle()  # create a circle
@@ -16,5 +15,25 @@ class SquareToCircle(Scene):
         square.rotate(PI / 4)  # rotate a certain amount
 
         self.play(Create(square))  # animate the creation of the square
-        self.play(Transform(square, circle))  # interpolate the square into the circle
-        self.play(FadeOut(square))  # fade out animation
+        #self.play(Transform(square, circle))  # interpolate the square into the circle
+        #self.play(FadeOut(square))  # fade out animation
+
+class ThreeCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        #circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+
+        circle2 = Circle()  # create a square
+        #circle2.set_fill(BLUE, opacity=0.5)  # set the color and transparency
+
+        circle3 = Circle()  # create a square
+        #circle3.set_fill(GREEN, opacity=0.5)  # set the color and transparency
+
+        circle2.next_to(circle, RIGHT, buff=0.5)  # set the position
+        circle3.next_to(circle, LEFT, buff=0.5)
+
+        self.play(circle.animate.set_fill(PINK, opacity=0.5))
+        self.play(circle2.animate.set_fill(BLUE, opacity=0.5))  
+        self.play(circle3.animate.set_fill(GREEN, opacity=0.5))  # set the color and transparency
+        
+        # show the shapes on screen
